9cb169ccae38f9ec42cbc7fc69a5043a19b8ea57
Flavio Pareja <flv.prj@gmail.com>
2018-05-09 10:16:10 -0500

    Actualizacion de diseño de model oculto

--- backend/psbox-m.php
+++ backend/psbox-m.php
@@ -1,15 +1,27 @@
 <?php
     echo "
-    <div class='fixed full-height full-width mod-ps no-margin' id='".$NUMID."'>
-        <div class='container-box principal-grey-bg box-pass raleway-font'>
-            <a href='#'>X</a>
-            <br>
-            <img class='image-account no-margin' src='../img/icons/add.png'>
-            <p class='extra-small no-margin'>#".$NUMID."</p>
-            <h4 class='no-margin'>".$NameAC."</h4>
-            <h5 class='no-margin'>".$UserAC."</h5>
-            <h5 class='no-margin'>".$Pass."</h5>
+        <div class='fixed full-height full-width mod-ps no-margin' id='".$NUMID."'>
+            <div class='container-box principal-grey-bg box-pass raleway-font'>
+                <a class='delete-button closes white' href='#'>X</a>
+                <div class='input-group inputs-add'>
+                    <span class='input-group-addon'>
+                    <span class='glyphicon glyphicon-pencil'></span>
+                    </span>
+                    <input class='form-control' disabled='true' name='ndc' placeholder='Nombre de cuenta' style='font-family:Arial, FontAwesome' type='text' value='".$NameAC."'>
+                </div>
+                <div class='input-group inputs-add'>
+                    <span class='input-group-addon'>
+                    <span class='glyphicon glyphicon-user'></span>
+                    </span>
+                    <input class='form-control' disabled='true' name='ndc' placeholder='Usuario' type='text' value='".$UserAC."'>
+                </div>
+                <div class='input-group inputs-add'>
+                    <span class='input-group-addon'>
+                    <span class='glyphicon glyphicon-lock'></span>
+                    </span>
+                    <input class='form-control' disabled='true' name='ndc' placeholder='Password' type='text' value='".$Pass."'>
+                </div>
+            </div>
         </div>
-    </div>
-    ";
+   ";
 ?>
